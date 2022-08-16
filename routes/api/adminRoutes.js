44b70c4ab567aef1246b3926e257dc693fab17f6
Bilel <hb.bilel@gmail.com>
2022-08-16 21:39:42 +0100

    exp

--- routes/api/adminRoutes.js
+++ routes/api/adminRoutes.js
@@ -1,19 +1,20 @@
-const express=require("express")
-const router=express.Router()
+const express = require("express");
+const router = express.Router();
 // const User=require("../../models/User");
-const adminController=require("../../controllers/adminController");
-const isAuth=require("../../middlewares/isAuth")
-const isAdmin = require("../../middlewares/isAdmin")
-
+const adminController = require("../../controllers/adminController");
+const isAuth = require("../../middlewares/isAuth");
+const isAdmin = require("../../middlewares/isAdmin");
 
 // path: http://localhost:5000/api/users/
 // get all users
 //
 
 // router.get("/",isAuth,isAdmin, adminController.getAdmins);
-router.get("/",isAuth, adminController.getAdmins).patch("/:id", isAuth, adminController.updateAdmin).patch("/delete/:id", adminController.deleteAdmin)
-router.get("/trainer",isAuth, adminController.getTrainer);
+router
+  .get("/", isAuth, adminController.getAdmins)
+  .patch("/:id", isAuth, adminController.updateAdmin)
+  .patch("/:id/delete", adminController.archiveAdmin) // archive
+  .delete("/:id", adminController.deleteAdmin); // delete
+router.get("/trainer", isAuth, adminController.getTrainer);
 
-
-
-module.exports=router;
+module.exports = router;
